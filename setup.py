9032be0ea07d1e2890e540a8be34006a09239b2f
mmatera <matera@fisica.unlp.edu.ar>
2021-03-30 16:44:12 -0300

    disabling processing glyphs as mtables...

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if sys.platform == "darwin":
 
 # General Requirements
 INSTALL_REQUIRES += [
-    "Mathics-Scanner >= 1.0.0,<1.1.0",
+    "Mathics-Scanner >= 1.0.0,<1.2.0",
     "Mathics3 >= 2.0.0",
     "django >= 3.0, < 3.2",
     "networkx >= 2.5",
