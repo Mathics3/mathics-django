1c200fba45cff63b0fa936b4befd1e1b43b8dcb9
rocky <rb@dustyfeet.com>
2021-04-20 23:43:53 -0400

    Black settings.py

--- mathics_django/settings.py
+++ mathics_django/settings.py
@@ -30,29 +30,27 @@ DEFAULT_MAX_RECURSION_DEPTH = 512
 # historically 10000 was used on public mathics servers
 MAX_STORED_SIZE = 10000
 
-ADMINS = (
-    ('Admin', 'mail@test.com'),
-)
+ADMINS = (("Admin", "mail@test.com"),)
 MANAGERS = ADMINS
 
-ROOT_DIR = pkg_resources.resource_filename('mathics_django', '')
-MATHICS_ROOT_DIR = pkg_resources.resource_filename('mathics', '')
-if sys.platform.startswith('win'):
-    DATA_DIR = os.environ['APPDATA'].replace(os.sep, '/') + '/Python/Mathics/'
+ROOT_DIR = pkg_resources.resource_filename("mathics_django", "")
+MATHICS_ROOT_DIR = pkg_resources.resource_filename("mathics", "")
+if sys.platform.startswith("win"):
+    DATA_DIR = os.environ["APPDATA"].replace(os.sep, "/") + "/Python/Mathics/"
 else:
-    DATA_DIR = path.expanduser('~/.local/var/mathics/')
+    DATA_DIR = path.expanduser("~/.local/var/mathics/")
 # if not path.exists(DATA_DIR):
 #    os.makedirs(DATA_DIR)
 
-DOC_DIR = os.path.join(MATHICS_ROOT_DIR, 'doc/documentation/')
-DOC_TEX_DATA = os.path.join(MATHICS_ROOT_DIR, 'doc/tex/data')
-DOC_XML_DATA = os.path.join(MATHICS_ROOT_DIR, 'doc/xml/data')
-DOC_LATEX_FILE = os.path.join(MATHICS_ROOT_DIR, 'doc/tex/documentation.tex')
+DOC_DIR = os.path.join(MATHICS_ROOT_DIR, "doc/documentation/")
+DOC_TEX_DATA = os.path.join(MATHICS_ROOT_DIR, "doc/tex/data")
+DOC_XML_DATA = os.path.join(MATHICS_ROOT_DIR, "doc/xml/data")
+DOC_LATEX_FILE = os.path.join(MATHICS_ROOT_DIR, "doc/tex/documentation.tex")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATA_DIR + 'mathics.sqlite'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATA_DIR + "mathics.sqlite",
     }
 }
 
@@ -60,9 +58,9 @@ REQUIRE_LOGIN = False
 
 
 # if REQUIRE_LOGIN is True be sure to set up an email sender:
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'mathics'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "mathics"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -72,14 +70,14 @@ EMAIL_USE_TLS = True
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Vienna'
+TIME_ZONE = "Europe/Vienna"
 
 # Set this True if you prefer 12 hour time to be the default
 TIME_12HOUR = False
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -92,13 +90,13 @@ ENABLE_FILES_MODULE = True
 USE_I18N = True
 
 # Absolute path to the directory that holds static files.
-STATIC_ROOT = os.path.join(ROOT_DIR, 'web/media/')
+STATIC_ROOT = os.path.join(ROOT_DIR, "web/media/")
 
 # URL that handles the media served from STATIC_ROOT.
-STATIC_URL = '/media/'
+STATIC_URL = "/media/"
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'uvbhuiasaeaph6Duh)r@3ex1i@et=0j4h(!p4@!r6s-=a_ev*e'
+SECRET_KEY = "uvbhuiasaeaph6Duh)r@3ex1i@et=0j4h(!p4@!r6s-=a_ev*e"
 
 # List of callables that know how to import templates from various sources.
 # TEMPLATE_LOADERS = (
@@ -107,12 +105,12 @@ SECRET_KEY = 'uvbhuiasaeaph6Duh)r@3ex1i@et=0j4h(!p4@!r6s-=a_ev*e'
 # )
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
 ]
 
-ROOT_URLCONF = 'mathics_django.urls'
+ROOT_URLCONF = "mathics_django.urls"
 
 # Rocky: this is probably a hack. LoadModule[] needs to handle
 # whatever it is that setting this thing did.
@@ -120,22 +118,20 @@ default_pymathics_modules = []
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(ROOT_DIR, 'web/templates/') ],
-        'OPTIONS': {
-            'debug': DEBUG,
-        }
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(ROOT_DIR, "web/templates/")],
+        "OPTIONS": {
+            "debug": DEBUG,
+        },
     }
 ]
 
-AUTHENTICATION_BACKENDS = (
-    'mathics.web.authentication.EmailModelBackend',
-)
+AUTHENTICATION_BACKENDS = ("mathics.web.authentication.EmailModelBackend",)
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'mathics_django.web',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "mathics_django.web",
 )
