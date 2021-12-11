0d92e07f1047b4426fa8d95b53879f86487e1186
Shiroki Satsuki <qinzezhao@foxmail.com>
2021-12-11 16:06:24 +0800

    Migrate from django.conf.urls.url to django.urls.re_path

--- mathics_django/urls.py
+++ mathics_django/urls.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-from django.conf.urls import url, include
+from django.urls import re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,5 +11,5 @@ handler500 = "mathics_django.web.views.error_500_view"
 
 urlpatterns = [
     # url(''),
-    url(r"^", include("mathics_django.web.urls")),
+    re_path(r"^", include("mathics_django.web.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
